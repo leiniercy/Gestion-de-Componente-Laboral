68f68700290d7bd1c33fb30f74dcf13d8c57156c
Leinier Caraballo <leiniercarballo08@gmail.com>
2022-02-27 16:10:56 -0500

    modifique el mensaje de error en el login

--- src/main/java/com/example/application/views/login/LoginView.java
+++ src/main/java/com/example/application/views/login/LoginView.java
@@ -7,6 +7,8 @@ package com.example.application.views.login;
 
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -16,35 +18,42 @@ import com.vaadin.flow.router.Route;
  */
 @Route("login")
 @PageTitle("Login")
-public class LoginView extends LoginOverlay {
+public class LoginView extends LoginOverlay implements BeforeEnterObserver {
 
     public LoginView() {
-        
+
         setAction("login");
 
         LoginI18n i18n = LoginI18n.createDefault();
-        
+
         i18n.setHeader(new LoginI18n.Header());
         i18n.getHeader().setTitle("GCL");
         i18n.getHeader().setDescription("Gestión Componente Laboral");
-        
+
         i18n.setAdditionalInformation(null);
-        
+
         i18n.getForm().setTitle("");
         i18n.getForm().setUsername("Usuario:");
         i18n.getForm().setPassword("Contraseña:");
         i18n.getForm().setSubmit("Iniciar sesión");
-        
-        
-        
-        LoginI18n.ErrorMessage errorMessage = i18n.getErrorMessage();
-        errorMessage.setTitle("Error de auntenticación");
-        errorMessage.setMessage("Usuario o contraseña incorrectos");
-        i18n.setErrorMessage(errorMessage);
-        setI18n(i18n);
 
+        i18n.getErrorMessage().setTitle("Error:");
+        i18n.getErrorMessage().setMessage("Usuario o contraseña incorrectos");
+
+        setI18n(i18n);
         setForgotPasswordButtonVisible(false);
         setOpened(true);
-        
     }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        // inform the user about an authentication error
+        if (beforeEnterEvent.getLocation()
+                .getQueryParameters()
+                .getParameters()
+                .containsKey("error")) {
+            setError(true);
+        }
+    }
+
 }
